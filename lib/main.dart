dfcac11b18d1d68109db10186aa246c0bc97c5ea
Srikar <t1srikar@gmail.com>
2023-03-12 15:06:04 +0530

    Added basic layout for Explore

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 // This is the main entry point for the application
 void main() => runApp(MyApp());
@@ -39,38 +40,41 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
   // The widgets that correspond to each tab
-  static List<Widget> _widgetOptions = <Widget>[
-    Center(
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Center(
       child: Text(
         'Home',
         style: TextStyle(fontSize: 30),
       ),
     ),
-    Center(
+    const Center(
       child: Text(
-        'Search',
+        'Explore',
         style: TextStyle(fontSize: 30),
       ),
     ),
-    Center(
+    const Center(
       child: Text(
         'Messages',
         style: TextStyle(fontSize: 30),
       ),
     ),
-    Center(
+    const Center(
       child: Text(
         'Notifications',
         style: TextStyle(fontSize: 30),
       ),
     ),
-    Center(
+    const Center(
       child: Text(
         'Profile',
         style: TextStyle(fontSize: 30),
       ),
     ),
   ];
+
+  // Random list of videos (to be changed later)
+  final List<int> _videoIndices = List.generate(50, (index) => Random().nextInt(100));
 
   // This function is called when a tab is tapped. It updates the index of the currently selected tab
   void _onItemTapped(int index) {
@@ -79,26 +83,55 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget _buildVideoGrid() {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      // The number of videos to display
+      itemCount: _videoIndices.length,
+      // The layout of the grid
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.47,
+      ),
+      // The widgets to display in the grid
+      itemBuilder: (BuildContext context, int index) {
+        // Return a blank video widget with a random number indicating the video index
+        return Container(
+          color: Colors.grey[300],
+          child: Center(
+            child: Text(
+              'Video ${_videoIndices[index]}',
+              style: const TextStyle(fontSize: 24),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // The scaffold provides the basic structure of the screen
     return Scaffold(
+      appBar: null, // hide the app bar
       // The body of the scaffold is the widget that corresponds to the currently selected tab
-      body: _widgetOptions.elementAt(_selectedIndex),
+      body: _selectedIndex == 1 ? _buildVideoGrid() : _widgetOptions.elementAt(_selectedIndex),
       // The bottom navigation bar shows the available tabs
       bottomNavigationBar: BottomNavigationBar(
         // The items in the bottom navigation bar correspond to each tab
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home_filled),
             label: 'Home',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            label: 'Search',
+            label: 'Explore',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.mail),
+            icon: Icon(Icons.message),
             label: 'Messages',
           ),
           BottomNavigationBarItem(
